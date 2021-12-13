0bf9dc0e4194e2dd52c739f2dbe41af0fe3aaf79
chokyeongbin <binyeong56@naver.com>
2021-12-14 03:44:38 +0900

    Data Preprocessing 2

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ from plotnine import *
 
 pre_sale = pd.read_csv('전국_평균_분양가격_2021.7월_.csv', sep='\t', engine='python', encoding='CP949') # 파일 불러오기
 
+d_list = pre_sale['지역명,규모구분,연도,월,분양가격(㎡)'].str.split(',')
+
+pre_sale['지역명'] = d_list.str.get(0)
+pre_sale['규모구분'] = d_list.str.get(1)
+pre_sale['연도'] = d_list.str.get(2)
+pre_sale['월'] = d_list.str.get(3)
+pre_sale['분양가격(㎡)'] = d_list.str.get(4)
+
+pre_sale_price = pre_sale['분양가격(㎡)']
+pre_sale['분양가격'] = pd.to_numeric(pre_sale_price, errors='coerce')
+pre_sale['평당분양가격'] = pre_sale['분양가격'] * 3.3
+
 print(pre_sale.head())
 print(pre_sale.info())
+print(pre_sale.describe())
+print(pre_sale['지역명'].value_counts())
+print(pre_sale['규모구분'].value_counts())
+print(pre_sale['연도'].value_counts())
